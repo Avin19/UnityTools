11a1be49b00b4602113e842644203877b6418485
Avin19 <avinashy1993@gmail.com>
2024-05-26 12:26:34 +0530

    Added custom scripts

--- CustomScriptsTemplate.cs
+++ CustomScriptsTemplate.cs
@@ -6,7 +6,19 @@ public static class CustomScriptsTemplets
     [MenuItem("Assets/Create/Code/MonoBehaviour", priority = 40)]
     public static void CreateMonoBehaviourMenuItem()
     {
-        string templatePath = "Assets/Editor/Templates/MonoBehaviour.cs.txt"
+        string templatePath = "Assets/Editor/Templates/NewScripts.cs.txt";
         ProjectWindowUtil.CreateScriptAssetFromTemplateFile(templatePath, "NewScript.cs");
+    }
+    [MenuItem("Assets/Create/Code/Enum", priority = 41)]
+    public static void CreateEnumMenuItem()
+    {
+        string templatePath = "Assets/Editor/Template/NewEnum.cs.txt";
+        ProjectWindowUtil.CreateScriptAssetFromTemplateFile(templatePath, "NewEnum.cs");
+    }
+    [MenuItem("Assets/Create/Code/ScriptableObject", priority = 42)]
+    public static void CreateScriptableObjectMenuItem()
+    {
+        string templatePath = "Assets/Editor/Template/NewScriptableObject.cs.txt";
+        ProjectWindowUtil.CreateScriptAssetFromTemplateFile(templatePath, "SONewScriptableObject.cs");
     }
 }
